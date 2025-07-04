9aef469b87cb9b0c779f06fbd3d3d1d4f657d187
iffinland <iffi@tuta.io>
2025-07-05 02:59:21 +0300

    minor fixes

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Impordi useNavigate
 
-function SearchBox({ placeholderText = "SEARCHING..." }) {
+function SearchBox({ placeholderText = "Type a search term to search for" }) {
   const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
 
